4f26378b4b7f0c8b40714e406247050c49707e4e
Christopher Fuentes <chris.fuentes@xamarin.com>
2015-12-08 12:25:06 -0800

    First Commit

---
+++ server/instrumentation-backend/src/com/jayway/android/robotium/solo/PublicViewFetcher.java
@@ -0,0 +1,11 @@
+package com.jayway.android.robotium.solo;
+
+import android.app.Activity;
+import android.app.Instrumentation;
+
+public class PublicViewFetcher extends ViewFetcher {
+
+    public PublicViewFetcher(ActivityUtils activityUtils) {
+        super(activityUtils);
+    }
+}

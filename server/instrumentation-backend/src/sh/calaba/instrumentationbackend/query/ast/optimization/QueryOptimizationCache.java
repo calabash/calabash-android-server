fdb4741a1d219de0ec12f00f441371c5d682f9b7
Tobias Roikjer <tobias.roikjer@xamarin.com>
2016-01-07 14:47:09 +0100

    QueryOptimizationCache: Use proper locks

--- server/instrumentation-backend/src/sh/calaba/instrumentationbackend/query/ast/optimization/QueryOptimizationCache.java
+++ server/instrumentation-backend/src/sh/calaba/instrumentationbackend/query/ast/optimization/QueryOptimizationCache.java
@@ -1,23 +1,38 @@
 package sh.calaba.instrumentationbackend.query.ast.optimization;
 
 import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import sh.calaba.instrumentationbackend.query.ast.UIQueryAST;
 
 public class QueryOptimizationCache {
     private static String key;
     private static List<UIQueryAST> value;
+    private static final Lock lock = new ReentrantLock();
 
-    public static synchronized List<UIQueryAST> getCacheFor(String key) {
-        if (QueryOptimizationCache.key != null && QueryOptimizationCache.key.equals(key)) {
-            return value;
-        } else {
-            return null;
+    public static List<UIQueryAST> getCacheFor(String key) {
+        lock.lock();
+
+        try {
+            if (QueryOptimizationCache.key != null && QueryOptimizationCache.key.equals(key)) {
+                return value;
+            } else {
+                return null;
+            }
+        } finally {
+            lock.unlock();
         }
     }
 
-    public static synchronized void cache(String key, List<UIQueryAST> value) {
-        QueryOptimizationCache.key = key;
-        QueryOptimizationCache.value = value;
+    public static void cache(String key, List<UIQueryAST> value) {
+        lock.lock();
+
+        try {
+            QueryOptimizationCache.key = key;
+            QueryOptimizationCache.value = value;
+        } finally {
+            lock.unlock();
+        }
     }
 }

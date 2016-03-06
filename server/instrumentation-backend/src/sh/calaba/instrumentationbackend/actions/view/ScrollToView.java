40dd03240f513febd419108d7266c90d26d5f141
Tobias Roikjer <tobias.roikjer@xamarin.com>
2016-03-06 16:15:47 +0100

    Scrolling: Problem: Query result no longer contains webcontainer
    Solution: Refactor to use our UIQueryMatcher.
    
    TODO: This commit introduces a new method Query.uiObjectsForQuery, this
    is temporary. We should refactor our code further to only return types of
    UIObject in QueryResult.

--- server/instrumentation-backend/src/sh/calaba/instrumentationbackend/actions/view/ScrollToView.java
+++ server/instrumentation-backend/src/sh/calaba/instrumentationbackend/actions/view/ScrollToView.java
@@ -5,9 +5,15 @@ import android.view.View;
 
 import sh.calaba.instrumentationbackend.Result;
 import sh.calaba.instrumentationbackend.actions.Action;
+import sh.calaba.instrumentationbackend.query.ViewMapper;
 import sh.calaba.instrumentationbackend.query.WebContainer;
+import sh.calaba.instrumentationbackend.query.ast.UIQueryMatcher;
 import sh.calaba.instrumentationbackend.query.ast.UIQueryUtils;
+import sh.calaba.instrumentationbackend.query.ui.UIObject;
+import sh.calaba.instrumentationbackend.query.ui.UIObjectView;
+import sh.calaba.instrumentationbackend.query.ui.UIObjectWebResult;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -24,18 +30,6 @@ public class ScrollToView implements Action, IOnViewAction {
     @Override
     public String key() {
         return "scroll_to_view";
-    }
-
-    @Override
-    public String doOnView(View view) {
-        scrollTo(view);
-        return "success";
-    }
-
-    @Override
-    public String doOnView(Map viewMap) {
-        scrollTo(viewMap);
-        return "success";
     }
 
     private void scrollTo(final View view) {
@@ -68,60 +62,83 @@ public class ScrollToView implements Action, IOnViewAction {
             });
         }
 
-    private void scrollTo(Map viewMap) {
-            final WebContainer webContainer = (WebContainer) viewMap.get("calabashWebContainer");
-            if (webContainer != null) {
-                final View view = webContainer.getView();
-                Map rectMap = (Map) viewMap.get("rect");
-                final int x = (Integer)rectMap.get("x");
-                final int y = (Integer)rectMap.get("y");
-                final int height = (Integer)rectMap.get("height");
-                final int width = (Integer) rectMap.get("width");
+    private void scrollTo(Map viewMap, WebContainer webContainer) {
+            final View view = webContainer.getView();
+            Map rectMap = (Map) viewMap.get("rect");
+            final int x = (Integer)rectMap.get("x");
+            final int y = (Integer)rectMap.get("y");
+            final int height = (Integer)rectMap.get("height");
+            final int width = (Integer) rectMap.get("width");
 
-                UIQueryUtils.runOnViewThread(view, new Runnable() {
-                    @Override
-                    public void run() {
-                        int[] webViewLocation = UIQueryUtils.getViewLocationOnScreen(view);
+            UIQueryUtils.runOnViewThread(view, new Runnable() {
+                @Override
+                public void run() {
+                    int[] webViewLocation = UIQueryUtils.getViewLocationOnScreen(view);
 
-                        int webViewHeight = view.getHeight();
-                        int webViewWidth = view.getWidth();
-                        int webviewY = webViewLocation[1];
-                        int webviewX = webViewLocation[0];
+                    int webViewHeight = view.getHeight();
+                    int webViewWidth = view.getWidth();
+                    int webviewY = webViewLocation[1];
+                    int webviewX = webViewLocation[0];
 
-                        int offsetY;
-                        if(webViewHeight > height) {
-                            int webviewBottom = webviewY + webViewHeight;
-                            int elementBottom = y + height;
-                            if(elementBottom > webviewBottom) {
-                                offsetY = elementBottom - webviewBottom;
-                            } else if(y < webviewY) {
-                                offsetY = y - webviewY;
-                            } else {
-                                offsetY = 0;
-                            }
+                    int offsetY;
+                    if(webViewHeight > height) {
+                        int webviewBottom = webviewY + webViewHeight;
+                        int elementBottom = y + height;
+                        if(elementBottom > webviewBottom) {
+                            offsetY = elementBottom - webviewBottom;
+                        } else if(y < webviewY) {
+                            offsetY = y - webviewY;
                         } else {
-                            int delta = height - webViewHeight;
-                            offsetY = y-webviewY + delta/2;
+                            offsetY = 0;
                         }
-
-                        int offsetX;
-                        if(webViewWidth > width) {
-                            int webviewRight = webviewX + webViewWidth;
-                            int elementRight = x + width;
-                            if(elementRight > webviewRight) {
-                                offsetX = elementRight - webviewRight;
-                            } else if(x < webviewX) {
-                                offsetX = x - webviewX;
-                            } else {
-                                offsetX = 0;
-                            }
-                        } else {
-                            int delta = width - webViewWidth;
-                            offsetX = x-webviewX + delta/2;
-                        }
-                        view.scrollBy(offsetX, offsetY);
+                    } else {
+                        int delta = height - webViewHeight;
+                        offsetY = y-webviewY + delta/2;
                     }
-                });
-            }
+
+                    int offsetX;
+                    if(webViewWidth > width) {
+                        int webviewRight = webviewX + webViewWidth;
+                        int elementRight = x + width;
+                        if(elementRight > webviewRight) {
+                            offsetX = elementRight - webviewRight;
+                        } else if(x < webviewX) {
+                            offsetX = x - webviewX;
+                        } else {
+                            offsetX = 0;
+                        }
+                    } else {
+                        int delta = width - webViewWidth;
+                        offsetX = x-webviewX + delta/2;
+                    }
+                    view.scrollBy(offsetX, offsetY);
+                }
+            });
         }
+
+    @Override
+    public UIQueryMatcher<String> getUIQueryMatcher(UIObject uiObject) {
+        return new Mapper(uiObject);
+    }
+
+    private class Mapper extends UIQueryMatcher<String> {
+        Mapper(UIObject uiObject) {
+            super(uiObject);
+        }
+
+        @Override
+        protected String matchForUIObject(UIObjectView uiObjectView) {
+            ScrollToView.this.scrollTo(uiObjectView.getObject());
+
+            return "success";
+        }
+
+        @Override
+        protected String matchForUIObject(UIObjectWebResult uiObjectWebResult) {
+            ScrollToView.this.scrollTo(uiObjectWebResult.getObject(),
+                    uiObjectWebResult.getWebContainer());
+
+            return "success";
+        }
+    }
 }

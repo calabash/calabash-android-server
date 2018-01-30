231b2d9b71449ed3989f57f945e0d39c2e7daf2c
Joshua Moody <joshuajmoody@gmail.com>
2018-01-30 11:46:57 +0100

    Bin: update tests use java 1.8
    was java 1.6

--- server/integration-tests/build_test_apk_fast.sh
+++ server/integration-tests/build_test_apk_fast.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/env bash
 
-JAVA_HOME=`/usr/libexec/java_home -v 1.6` $ANDROID_HOME/build-tools/23.0.1/dx --dex --output "4_Calabash.jar"  "libs/build/Calabash.jar"
-JAVA_HOME=`/usr/libexec/java_home -v 1.6` $ANDROID_HOME/build-tools/23.0.1/dx --dex --output "7_CalabashIntegrationTests.jar"  "out/CalabashIntegrationTests.jar"
 set -e
 
+JAVA_HOME=`/usr/libexec/java_home -v 1.8` $ANDROID_HOME/build-tools/26.0.2/dx --dex --output "4_Calabash.jar"  "libs/build/Calabash.jar"
+JAVA_HOME=`/usr/libexec/java_home -v 1.8` $ANDROID_HOME/build-tools/26.0.2/dx --dex --output "7_CalabashIntegrationTests.jar"  "out/CalabashIntegrationTests.jar"
 adb push 4_Calabash.jar /sdcard/jars/
 adb push 7_CalabashIntegrationTests.jar /sdcard/jars/
 rm 4_Calabash.jar

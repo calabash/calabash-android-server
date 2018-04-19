3008086adb6ed227cf30bd4048c20b9cec35d771
Andrey Mitsyk (Akvelon) <v-anmits@microsoft.com>
2018-04-19 15:41:57 +0300

    Added separate run script for Jenkins.

---
+++ bin/ci/jenkins/run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+bin/build.sh
+cd server/integration-tests
+./run_and_compile.sh
--- bin/ci/travis/run.sh
+++ bin/ci/travis/run.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
 bin/build.sh
-# Currently integration tests can't be started in Travis.
-#cd server/integration-tests
-#./run_and_compile.sh

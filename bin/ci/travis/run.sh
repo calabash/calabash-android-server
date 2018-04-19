8d45060596c924c5d9e238ea5b1edb85049fb755
Andrey Mitsyk (Akvelon) <v-anmits@microsoft.com>
2018-04-19 15:03:30 +0300

    Disable integration tests in Travis, but enable it in Jarvis.

--- bin/ci/travis/run.sh
+++ bin/ci/travis/run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 bin/build.sh
-cd server/integration-tests
-./run_and_compile.sh
+# Currently integration tests can't be started in Travis.
+#cd server/integration-tests
+#./run_and_compile.sh

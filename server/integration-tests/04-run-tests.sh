de45c62cd822bd73fd3b6895f05d3316982a15f6
Joshua Moody <josmoo@microsoft.com>
2019-07-11 14:20:59 +0200

    gradle: verify app as part of prepareApk task

--- server/integration-tests/04-run-tests.sh
+++ server/integration-tests/04-run-tests.sh
@@ -8,6 +8,10 @@ pushd calabash-test-suite > /dev/null
   bundle update
   bundle exec calabash-android resign unit-tester.apk
   bundle exec calabash-android build unit-tester.apk
+
+  # The app will pop a sh.calaba.unittester "built for older version of Android"
+  # dialog.  This can be ignored - it does not influence the success or failure
+  # of the tests.
   SKIP_VERSION_CHECK=1 bundle exec calabash-android run \
     unit-tester.apk \
     --format pretty \

64ecefe85ea91c653c3f0e0094169d48d4bb4d2b
Joshua Moody <josmoo@microsoft.com>
2019-07-12 14:59:08 +0200

    ado: simplify the install-android-sdk.sh

--- bin/ci/ado/install-android-sdk.sh
+++ bin/ci/ado/install-android-sdk.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 
-uname -a
-$ANDROID_HOME/tools/bin/sdkmanager --version
+pushd "${ANDROID_HOME}/tools" > /dev/null
+  bin/sdkmanager --version
+  echo "y" | bin/sdkmanager --update
 
-cd $ANDROID_HOME/tools
-
-echo "y" | bin/sdkmanager --update
-echo "y" | bin/sdkmanager --install tools
-echo "y" | bin/sdkmanager --install platform-tools
-echo "y" | bin/sdkmanager --install emulator
-echo "y" | bin/sdkmanager --install 'platforms;android-28'
-echo "y" | bin/sdkmanager --install 'build-tools;28.0.3'
-echo "y" | bin/sdkmanager --install 'system-images;android-28;google_apis;x86'
+  echo "y" | \
+  bin/sdkmanager --install tools platform-tools emulator 'platforms;android-28' \
+    'build-tools;28.0.3' 'system-images;android-28;google_apis;x86'
+popd > /dev/null

41184377b91c8b8b137826ae155248b33b79297b
Ivan Pochtar <ivan.pochtar@corp.badoo.com>
2020-07-13 12:35:38 +0100

    verify actions

---
+++ server/app/src/androidTest/java/sh/calaba/instrumentationbackend/actions/device/Actions.java
@@ -0,0 +1,24 @@
+package sh.calaba.instrumentationbackend.actions.device;
+
+public enum Actions {
+    click,
+    longClick,
+    getText,
+    getContentDescription,
+    getClassName,
+    getResourceName,
+    getVisibleBounds,
+    getVisibleCenter,
+    getApplicationPackage,
+    getChildCount,
+    clear,
+    isCheckable,
+    isChecked,
+    isClickable,
+    isEnabled,
+    isFocusable,
+    isFocused,
+    isLongClickable,
+    isScrollable,
+    isSelected;
+}

418eee5fc5af4df026b295610fd2ed46646f6280
Júlia Garrigós <julia.garrigos@xing.com>
2022-06-17 21:30:37 +0200

    Scrolling actions fixed
    Two actions have been added: one for vertical and one for horizontal scrolling. Both of them receive the following parameters: strategy and locator to find the target view, optionally, the strategy and locator to find the container view and finally the max number of scrolls that we want to do before stop trying to find the element and returning an error.

---
+++ server/app/src/androidTest/java/sh/calaba/instrumentationbackend/actions/device/UiautomatorVerticallyScrollToElement.java
@@ -0,0 +1,48 @@
+package sh.calaba.instrumentationbackend.actions.device;
+
+import java.lang.reflect.InvocationTargetException;
+
+import androidx.test.uiautomator.UiObjectNotFoundException;
+import sh.calaba.instrumentationbackend.InstrumentationBackend;
+import sh.calaba.instrumentationbackend.Result;
+import sh.calaba.instrumentationbackend.actions.Action;
+
+import static sh.calaba.instrumentationbackend.actions.device.ScrollToElementActionHelper.scrollToTargetInContainer;
+
+public class UiautomatorVerticallyScrollToElement implements Action {
+    @Override
+    public Result execute(String... args) {
+        InstrumentationBackend.getUiDevice();
+        try {
+            String targetBySelectorStrategy = args[0];
+            String targetLocator = args[1];
+
+            String containerBySelectorStrategy = null;
+            String containerLocator = null;
+            if (args.length >= 4) {
+                containerBySelectorStrategy = args[2];
+                containerLocator = args[3];
+            }
+            int maxScrolls = 10;
+            if (args.length >= 5) {
+                maxScrolls = Integer.parseInt(args[4]);
+            }
+
+            scrollToTargetInContainer(targetBySelectorStrategy, targetLocator, containerBySelectorStrategy,
+                  containerLocator, maxScrolls, false);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (UiObjectNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+        return new Result(true);
+    }
+
+    @Override
+    public String key() { return "uiautomator_vertically_scroll_to_element"; }
+}
+
